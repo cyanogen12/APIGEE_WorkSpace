72ade39e3de7fd78d4fad6331fd5c6fd1755267b
Vivek Yadav 2 <vyad23@sapient.com>
2017-08-01 16:03:19 +0530

    chhanged description

--- scr/gateway/JSMashup/target/apiproxy/resources/jsc/ChangePathSuffix.js
+++
@@ -1,5 +0,0 @@
- url =context.getVariable("target.url");
- 
- var newurl=url+"/get"
- 
- context.setVariable("target.url",newurl);
--- scr/gateway/JSMashup/target/apiproxy/resources/jsc/RewPinCodesJS.js
+++
@@ -1,9 +0,0 @@
- if (request.queryParams.postalcode === undefined){
-     throw '"PinCode" query parameter is required';
-   }
- else{
-    var pincode=request.queryParams.pincode;
-    var pincodeDetails=httpClient.get("http://postalpincode.in/api/pincode/122004");
-    context.session["pinCodeObj"]= pincodeDetails;
- }
- 
